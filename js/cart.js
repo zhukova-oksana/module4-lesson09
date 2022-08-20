94958b82e47b8fe6dc8ef2055284a60c66f3b84e
zhukova_oksana <ksy2905@gmail.com>
2022-08-20 08:39:24 +0300

    error eslint

--- js/cart.js
+++ js/cart.js
@@ -5,44 +5,43 @@ const cart = {
   // totalPrice: 0,
   sum: 0,
   count: 0,
-  add (nameProduct, priceProduct, amountProduct = 1) {
+  add(nameProduct, priceProduct, amountProduct = 1) {
     const product = {
       name: nameProduct,
       price: priceProduct,
       amount: amountProduct,
-    }
+    };
 
     this.items.push(product);
     this.increaseCount(product.amount);
     this.totalPrice;
   },
-  increaseCount (num) {
+  increaseCount(num) {
     return this.count += num;
   },
-  calculateItemPrice () {
+  calculateItemPrice() {
     let sum = 0;
     this.items.forEach((item) => {
       sum += item.amount * item.price;
     });
     return sum;
   },
-  clear () {
+  clear() {
     this.items = [];
     // this.totalPrice;
     this.count = 0;
   },
-  print () {
+  print() {
     console.log(JSON.stringify(this.items));
     console.log('Общая стоимость:', this.totalPrice);
   },
   get totalPrice() {
-      return this.calculateItemPrice();
+    return this.calculateItemPrice();
   },
-}
+};
 
 cart.add('стул', 400, 11);
 cart.add('стол', 500);
 cart.add('лампа', 200, 5);
-
 
 cart.print();
